8f74bf97ac2fc5637192c822e54a7f14bf332ee0
shinyorke <spirits.is.my.rader@gmail.com>
2015-11-28 16:24:55 +0900

    create scraping script for melba.py

--- pitchpx/mlbam.py
+++ pitchpx/mlbam.py
@@ -6,12 +6,16 @@ import click
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from formencode import validators
+from datetime import datetime as dt
+from datetime import timedelta
 import yaml
 
 __author__ = 'Shinichi Nakagawa'
 
 
 class MlbAm(object):
+
+    DATE_FORMAT = '%Y%m%d'
 
     def __init__(self, base_dir, setting_file='setting.yml'):
         self.setting = yaml.load(open('/'.join([base_dir, setting_file]), 'r'))
@@ -31,7 +35,7 @@ class MlbAm(object):
         datetime_check = validators.Int()
         datetime_check.to_python(value)
         if len(value) != 8:
-            raise MlbAmException("Length Error:{value}({length})".format(value=value, length=len(value)))
+            raise MlbAmBadParameter("Length Error:{value}({length})".format(value=value, length=len(value)))
 
     @classmethod
     def _validate_datetime_from_to(cls, start, end):
@@ -42,7 +46,7 @@ class MlbAm(object):
         :return: None or MlbAmException
         """
         if not start <= end:
-            raise MlbAmException("not Start Day({start}) <= End Day({end})".format(start=start, end=end))
+            raise MlbAmBadParameter("not Start Day({start}) <= End Day({end})".format(start=start, end=end))
 
     @classmethod
     def scrape(cls, start, end):
@@ -52,11 +56,22 @@ class MlbAm(object):
         :param end: End Day(YYYYMMDD)
         """
 
+        # validate
         for param_day in ({'name': 'Start Day', 'value': start}, {'name': 'End Day', 'value': end}):
             try:
                 cls._validate_datetime(param_day['value'])
             except (validators.Invalid, MlbAmException) as e:
-                raise click.BadParameter('{msg} a {name}.'.format(name=param_day['name'], msg=e.msg))
+                raise MlbAmException('{msg} a {name}.'.format(name=param_day['name'], msg=e.msg))
+        cls._validate_datetime_from_to(start, end)
+
+        # datetime
+        start_day, end_day = dt.strptime(start, cls.DATE_FORMAT), dt.strptime(end, cls.DATE_FORMAT)
+        delta = end_day - start_day
+
+        # TODO 並列化に備えてここで日付リストをとっておく
+        days = []
+        for day in range(delta.days+1):
+            days.append(start_day + timedelta(days=day))
 
         mlb = MlbAm(os.path.dirname(os.path.abspath('.')))
         mlb.get()
@@ -67,6 +82,10 @@ class MlbAmException(Exception):
     def __init__(self, msg):
         Exception.__init__(self, msg)
         self.msg = msg
+
+
+class MlbAmBadParameter(MlbAmException):
+    pass
 
 
 @click.command()
@@ -80,8 +99,8 @@ def scrape(start, end):
     """
     try:
         MlbAm.scrape(start, end)
-    except MlbAmException as e:
-        raise click.BadParameter('{msg} a {name}.'.format(name=param_day['name'], msg=e.msg))
+    except MlbAmBadParameter as e:
+        raise click.BadParameter(e)
 
 if __name__ == '__main__':
     scrape()
