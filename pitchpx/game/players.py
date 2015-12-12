91d5ea96595ba3a5d2e265019b91a4498726fc17
shinyorke <spirits.is.my.rader@gmail.com>
2015-12-12 21:12:09 +0900

    refactoring for baseball people struct(player, coach, umpire)

--- pitchpx/game/players.py
+++ pitchpx/game/players.py
@@ -8,110 +8,6 @@ from pitchpx.game.game import Game
 __author__ = 'Shinichi Nakagawa'
 
 
-class YakyuMin(object):
-    """
-    Baseball people(Japanese "やきう民")
-    """
-    id = MlbamConst.UNKNOWN_FULL
-    first = MlbamConst.UNKNOWN_FULL
-    last = MlbamConst.UNKNOWN_FULL
-    position = MlbamConst.UNKNOWN_SHORT
-
-    def __init__(self, soup):
-        """
-        create object
-        :param soup: Beautifulsoup object
-        """
-        self.id = soup['id']
-        self.first = soup['first']
-        self.last = soup['last']
-        self.position = soup['position']
-
-
-class Player(YakyuMin):
-    """
-    Player's Data(Pitcher/Batter)
-    """
-    num = MlbamConst.UNKNOWN_FULL
-    box_name = MlbamConst.UNKNOWN_FULL
-    rl = MlbamConst.UNKNOWN_SHORT
-    bats = MlbamConst.UNKNOWN_SHORT
-    status = MlbamConst.UNKNOWN_FULL
-    team_abbrev = MlbamConst.UNKNOWN_FULL
-    team_id = MlbamConst.UNKNOWN_FULL
-    parent_team_abbrev = MlbamConst.UNKNOWN_FULL
-    parent_team_id = MlbamConst.UNKNOWN_FULL
-    avg = 0.0
-    hr = 0
-    rbi = 0
-    wins = 0
-    losses = 0
-    era = 0.0
-
-    def __init__(self, soup):
-        """
-        create object
-        :param soup: Beautifulsoup object
-        """
-        super().__init__(soup)
-        self.num = int(soup['num'])
-        self.box_name = soup['boxname']
-        self.rl = soup['rl']
-        self.bats = soup['bats']
-        self.status = soup['status']
-        self.team_abbrev = soup['team_abbrev']
-        self.team_id = soup['team_id']
-        self.parent_team_abbrev = soup['parent_team_abbrev']
-        self.parent_team_id = soup['parent_team_id']
-        if 'avg' in soup.attrs:
-            self.avg = float(soup['avg'])
-        if 'hr' in soup.attrs:
-            self.hr = int(soup['hr'])
-        if 'rbi' in soup.attrs:
-            self.rbi = int(soup['rbi'])
-        if 'wins' in soup.attrs:
-            self.wins = int(soup['wins'])
-        if 'losses' in soup.attrs:
-            self.losses = int(soup['losses'])
-        if 'era' in soup.attrs:
-            self.era = float(soup['era'])
-
-
-class Coach(YakyuMin):
-    """
-    Coach Data
-    """
-    num = MlbamConst.UNKNOWN_FULL
-    team_id = MlbamConst.UNKNOWN_SHORT
-    team_name = MlbamConst.UNKNOWN_FULL
-
-    def __init__(self, soup, team):
-        """
-        create object
-        :param soup: Beautifulsoup object
-        :param team: Team object
-        """
-        super().__init__(soup)
-        self.num = int(soup['num'])
-        self.team_id = team.id
-        self.team_name = team.name
-
-
-class Umpire(YakyuMin):
-    """
-    Umpire Data
-    """
-    name = MlbamConst.UNKNOWN_FULL
-
-    def __init__(self, soup):
-        """
-        create object
-        :param soup: Beautifulsoup object
-        """
-        super().__init__(soup)
-        self.name = soup['name']
-
-
 class Players(object):
 
     FILENAME = 'players.xml'
@@ -121,6 +17,25 @@ class Players(object):
     home_team = None
     away_team = None
     game = None
+
+    class YakyuMin(object):
+        """
+        Baseball people(Japanese "やきう民")
+        """
+        id = MlbamConst.UNKNOWN_FULL
+        first = MlbamConst.UNKNOWN_FULL
+        last = MlbamConst.UNKNOWN_FULL
+        position = MlbamConst.UNKNOWN_SHORT
+
+        def __init__(self, soup):
+            """
+            create object
+            :param soup: Beautifulsoup object
+            """
+            self.id = soup['id']
+            self.first = soup['first']
+            self.last = soup['last']
+            self.position = soup['position']
 
     class Team(object):
         """
@@ -136,6 +51,87 @@ class Players(object):
         """
         venue = MlbamConst.UNKNOWN_FULL
         date = MlbamConst.UNKNOWN_FULL
+
+    class Player(YakyuMin):
+        """
+        Player's Data(Pitcher/Batter)
+        """
+        num = MlbamConst.UNKNOWN_FULL
+        box_name = MlbamConst.UNKNOWN_FULL
+        rl = MlbamConst.UNKNOWN_SHORT
+        bats = MlbamConst.UNKNOWN_SHORT
+        status = MlbamConst.UNKNOWN_FULL
+        team_abbrev = MlbamConst.UNKNOWN_FULL
+        team_id = MlbamConst.UNKNOWN_FULL
+        parent_team_abbrev = MlbamConst.UNKNOWN_FULL
+        parent_team_id = MlbamConst.UNKNOWN_FULL
+        avg = 0.0
+        hr = 0
+        rbi = 0
+        wins = 0
+        losses = 0
+        era = 0.0
+
+        def __init__(self, soup):
+            """
+            create object
+            :param soup: Beautifulsoup object
+            """
+            super().__init__(soup)
+            self.num = int(soup['num'])
+            self.box_name = soup['boxname']
+            self.rl = soup['rl']
+            self.bats = soup['bats']
+            self.status = soup['status']
+            self.team_abbrev = soup['team_abbrev']
+            self.team_id = soup['team_id']
+            self.parent_team_abbrev = soup['parent_team_abbrev']
+            self.parent_team_id = soup['parent_team_id']
+            if 'avg' in soup.attrs:
+                self.avg = float(soup['avg'])
+            if 'hr' in soup.attrs:
+                self.hr = int(soup['hr'])
+            if 'rbi' in soup.attrs:
+                self.rbi = int(soup['rbi'])
+            if 'wins' in soup.attrs:
+                self.wins = int(soup['wins'])
+            if 'losses' in soup.attrs:
+                self.losses = int(soup['losses'])
+            if 'era' in soup.attrs:
+                self.era = float(soup['era'])
+
+    class Coach(YakyuMin):
+        """
+        Coach Data
+        """
+        num = MlbamConst.UNKNOWN_FULL
+        team_id = MlbamConst.UNKNOWN_SHORT
+        team_name = MlbamConst.UNKNOWN_FULL
+
+        def __init__(self, soup, team):
+            """
+            create object
+            :param soup: Beautifulsoup object
+            :param team: Team object
+            """
+            super().__init__(soup)
+            self.num = int(soup['num'])
+            self.team_id = team.id
+            self.team_name = team.name
+
+    class Umpire(YakyuMin):
+        """
+        Umpire Data
+        """
+        name = MlbamConst.UNKNOWN_FULL
+
+        def __init__(self, soup):
+            """
+            create object
+            :param soup: Beautifulsoup object
+            """
+            super().__init__(soup)
+            self.name = soup['name']
 
     def __init__(self):
         self.game = self.Game()
@@ -165,12 +161,12 @@ class Players(object):
                 # team data(away)
                 players.away_team = team_object
             # player data
-            players.rosters.update({player['id']: Player(player) for player in team.find_all('player')})
+            players.rosters.update({player['id']: cls.Player(player) for player in team.find_all('player')})
             # coach data
-            players.coaches.update({coach['id']: Coach(coach, team_object) for coach in team.find_all('coach')})
+            players.coaches.update({coach['id']: cls.Coach(coach, team_object) for coach in team.find_all('coach')})
         # umpire data
         umpires = soup.find('umpires')
-        players.umpires.update({umpire['id']: Umpire(umpire) for umpire in umpires.find_all('umpire')})
+        players.umpires.update({umpire['id']: cls.Umpire(umpire) for umpire in umpires.find_all('umpire')})
         return players
 
     @classmethod
