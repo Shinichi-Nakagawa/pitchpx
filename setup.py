804d9629270c9018b7d132e8c69a716c8fb0773d
shinyorke <spirits.is.my.rader@gmail.com>
2016-01-04 01:06:44 +0900

    Change to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
 
 __author__ = 'Shinichi Nakagawa'
+
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    return ''
 
 
 setup(
     name='pitchpx',
     version='1.2',
     description='Tools for Acquiring MLBAM Gameday dataset',
+    long_description=read_file('README.rst'),
     author='Shinichi Nakagawa',
     author_email='spirits.is.my.rader@gmail.com',
+    url='https://github.com/Shinichi-Nakagawa/pitchpx',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
     packages=find_packages(),
     include_package_data=True,
+    keywords=['baseball', 'MLB', 'MLBAM'],
+    license='MIT License',
     install_requires=[
         'beautifulsoup4',
         'click',
